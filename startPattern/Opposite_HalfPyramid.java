c460340ea2a4bcba4f72508e7707bcedff70f2b3
guptaUd97 <159722038+guptaUd97@users.noreply.github.com>
2024-07-08 19:56:30 +0530

    star pattern

---
+++ startPattern/Opposite_HalfPyramid.java
@@ -0,0 +1,34 @@
+package com.startPattern;
+
+public class Opposite_HalfPyramid {
+	
+	/*
+	 *  - - - *
+	 * 	- - * *
+	 * 	- * * *
+	 * 	* * * *
+	 */
+	
+	public static void main(String[] args) {
+		
+		int n=4;
+		
+		//outer for loop
+		for(int i=1; i<=n; i++) {
+			
+			//inner loop space print
+			for(int j=1; j<= n-i; j++) {
+				System.out.print(" ");
+			}
+			
+			//inner loop star print
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
